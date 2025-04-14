bac61b0ba64663b68a07acdc64f4b4e9d91e9e74
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2025-04-14 14:32:26 +0200

    Add dockerfile and variable debug

--- flask-app/app.py
+++ flask-app/app.py
@@ -31,4 +31,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    debug=os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    app.run(debug, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))

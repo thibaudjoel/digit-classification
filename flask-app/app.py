319966165b5c4d608a12c860f05e88d720fc940a
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2025-04-15 17:07:28 +0200

    Make form work and show success message with styling

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import tensorflow as tf
-
 from utils import set_up_dirs, standardize_img
-import io
 import os
 import uuid
 import json
@@ -15,11 +13,12 @@ model = tf.keras.models.load_model("model.keras")
 
 set_up_dirs()
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         img = standardize_img(request.json["image"])
-        np_array = np.array(img) / 255.0 # in model
+        np_array = np.array(img) / 255.0  # in model
         np_array = np_array.reshape(1, 28, 28, 1)
 
         prediction = np.argmax(model.predict(np_array)[0])
@@ -30,10 +29,11 @@ def index():
 @app.route("/labeling", methods=["GET", "POST"])
 def labeling():
     if request.method == "POST":
-        print("OK")
-        img = standardize_img(request.json["image"])
-        digit = int(request.json["digit"])
-        
+        digit = request.json.get("digit")
+        drawing = request.json.get("drawing")
+        img = standardize_img(drawing)
+        digit = int(digit)
+
         # Save it to a file
         filename = f"{uuid.uuid4()}.png"
         img.save(f"labeled_data/imgs/{filename}")
@@ -52,5 +52,5 @@ def labeling():
 
 
 if __name__ == "__main__":
-    debug = True #os.environ.get("FLASK_DEBUG", "true").lower() == "true"
+    debug = True  # os.environ.get("FLASK_DEBUG", "true").lower() == "true"
     app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
